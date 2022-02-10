f698f6c8ce21a80f17065532e06ef9f92cff1c16
brayan <brayan.11fera@gmail.com>
2022-02-10 16:23:10 -0300

    Adicionando a base do projeto que foi gerada no Spring Intlzr

---
+++ api/godebts/src/main/java/br/com/godebts/GodebtsApplication.java
@@ -0,0 +1,13 @@
+package br.com.godebts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodebtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodebtsApplication.class, args);
+	}
+
+}
---
+++ api/godebts/src/test/java/br/com/godebts/GodebtsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.godebts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodebtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
