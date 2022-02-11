832e439656f8e10a95daf4989291eda1835f75a9
brayan <brayan.11fera@gmail.com>
2022-02-11 10:53:26 -0300

    Configuração do pom.xml e também do application.properties da aplicação

---
+++ api/godebts/src/main/java/br/com/godebts/model/Usuario.java
@@ -0,0 +1,12 @@
+package br.com.godebts.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Usuario {
+}
